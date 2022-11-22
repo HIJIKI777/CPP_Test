278af478bf5dfadec8e7aa9c083380e717e3bdd5
HIJIKI777 <kuboyoshi0318@icloud.com>
2022-11-22 23:31:27 +0900

    map test

---
+++ knowledge/map.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<map>
+#include<string>
+
+using namespace std;
+
+int main(){
+
+    map<string,int>student_score;
+    student_score = {{"nishikubo",100}, {"aitsu",1000},{"omae",1}};
+
+    return 0;
+}
