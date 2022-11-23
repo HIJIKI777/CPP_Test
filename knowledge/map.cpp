8d91363cffead3be6ee1d9a41938dbaed98a6a34
HIJIKI777 <kuboyoshi0318@icloud.com>
2022-11-24 03:56:28 +0900

    map reviced

--- knowledge/map.cpp
+++ knowledge/map.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<map>
 #include<string>
+#include<algorithm>
 
 using namespace std;
 
@@ -8,6 +9,21 @@ int main(){
 
     map<string,int>student_score;
     student_score = {{"nishikubo",100}, {"aitsu",1000},{"omae",1}};
+    student_score["kare"] = 900;
+    cout << student_score.size() << endl;
+    cout << student_score.at("kare") << endl;
+
+    for_each(student_score.begin(),student_score.end(),[](pair<string,int>i){
+        cout << i.first << " " << i.second << endl;
+    });
+
+    //autoって便利やな
+    auto itr = student_score.find("kare");
+    student_score.erase(itr);
+
+    for_each(student_score.begin(),student_score.end(),[](pair<string,int>i){
+        cout << i.first << " " << i.second << endl;
+    });
 
     return 0;
 }
